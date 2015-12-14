7044520e24cb05279404d56a098b8bbc87c36cd5
aolgu003 <aolgu003@ucr.edu>
2015-12-14 15:09:02 -0800

    Reorganized directories and added new segmentation algorithms

---
+++ vision/segmentation/CMakeFiles/saliencySeg.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/saliencySeg.dir/sailencySeg.cpp.o"
+  "saliencySeg.pdb"
+  "saliencySeg"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/saliencySeg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
